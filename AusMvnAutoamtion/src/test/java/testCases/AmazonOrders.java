b61fe2aef222834366206061aacfd4898c6c5bc2
selenium454 <jcreddy25@gmail.com>
2019-03-22 12:04:23 +1000

    unwanted files removed

--- AusMvnAutoamtion/src/test/java/testCases/AmazonOrders.java
+++ AusMvnAutoamtion/src/test/java/testCases/AmazonOrders.java
@@ -2,15 +2,10 @@ package testCases;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import reports.AdvExtentReports;
