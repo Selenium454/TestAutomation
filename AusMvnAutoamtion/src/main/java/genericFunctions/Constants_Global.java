eaca10b8a30511cff4080228a791db0f1cb5b7f3
selenium454 <jcreddy25@gmail.com>
2019-04-23 11:36:59 +1000

    change done

--- AusMvnAutoamtion/src/main/java/genericFunctions/Constants_Global.java
+++ AusMvnAutoamtion/src/main/java/genericFunctions/Constants_Global.java
@@ -6,7 +6,7 @@ public class Constants_Global {
 	public static String firefoxKey = "webdriver.gecko.driver";
 	public static String ieKey = "webdriver.ie.driver";
 
-	public static String chromePath = "./DriverFiles/chromedriver.exe";
+	public static String chromePath = System.getProperty("user.dir")+"/DriverFiles/chromedriver.exe";
 	public static String firefoxPath = "./DriverFiles/geckodriver.exe";
 	public static String iePath = "./DriverFiles/IEDriverServer.exe";
 	
